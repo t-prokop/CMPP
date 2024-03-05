31dc3e52ee0f7384fbf8e85572d981bddd3f7cb9
t-prokop <t.prokop@student.uw.edu.pl>
2024-03-05 14:09:39 +0100

    skonczone w domu

--- zad1/t2_life.py
+++ zad1/t2_life.py
@@ -1,48 +1,70 @@
-#%%
+# %%
+from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("/nfs/work/2022/tp449528/lib")
-from numba import jit
 
-@jit(nopython = True)
-def evlove(n_val, curr):
-    if n_val == 3:
-        return 1
-    elif n_val == 2 and curr == 1:
-        return 1
-    else:
-        return 0
-print("xd")
-#TASK 2
-#init
-life_shape = (256,512)
+# TASK 2
+# init
+life_shape = (256, 512)
+# life_shape = (8, 8)
 L = life_shape[1]
-life = np.random.randint(0,2,life_shape)
-T_max = 1000
+life = np.random.randint(0, 2, life_shape)
+T_max = 10000
+boundary = np.zeros(life_shape)
+for i in range(life_shape[0]):
+    boundary[i, 0] = 1
+    boundary[i, -1] = 1
+for i in range(life_shape[1]):
+    boundary[0, i] = 1
+    boundary[-1, i] = 1
+# %%
 
-print("xdxd")
-#%%
-def save(time, life, life_shape):
-    fig = life.reshape(life_shape)
-    plt.imshow(fig, interpolation = 'none')
-    plt.savefig(f"./zad1/imgs/life_{time}.png")
 
-@jit(nopython = True)
-def calc_neighbour(life,L):
-    neighbour_val = np.roll(life,1) + np.roll(life,-1) \
-            + np.roll(life,L) + np.roll(life,-L) \
-            + np.roll(life,L+1) + np.roll(life,L-1)\
-            + np.roll(life,-L+1) + np.roll(life,-L-1)
+def save(time, life, filenames):
+    plt.imshow(life, interpolation='none')
+    plt.savefig(f"./imgs/life_{time}.png")
+    filenames.append(f"./imgs/life_{time}.png")
+
+
+@jit(nopython=True)
+def calc_neighbour(life, L):
+    life = life.ravel()
+    neighbour_val = np.roll(life, -1) + np.roll(life, 1) + np.roll(life, L) + np.roll(
+        life, -L) + np.roll(life, L+1) + np.roll(life, -L+1) + np.roll(life, L-1) + np.roll(life, -L-1)
+    # neighbour_val = np.roll(life, (1,0), axis=(0,1)) + np.roll(life, (-1,0), axis=(0,1)) \
+    #     + np.roll(life, (0,-1), axis=(0,1)) + np.roll(life, (0,1), axis=(0,1)) \
+    #     + np.roll(life, (1,1), axis=(0,1)) + np.roll(life, (-1,1), axis=(0,1))\
+    #     + np.roll(life, (-1,-1), axis=(0,1)) + np.roll(life, (1,-1), axis=(0,1))
     return neighbour_val
 
 
-def run(time, life,L, life_shape):
+# @jit(nopython=True)
+def calc_next_frame(life, L, boundary):
+    n_vals = calc_neighbour(life, L)
+    n_vals = n_vals.reshape(life.shape)
+    newlife = (n_vals == 3) | ((n_vals == 2) & (life == 1)) | (boundary == 1)
+    return newlife.astype(int)
+
+
+def run(time, life, L, boundary, filenames):
+
     for t in range(time):
-        n_vals = calc_neighbour(life,L)
+        if t % 100 == 0:
+            save(t, life, filenames)
 
-        life = np.array(list(map(evlove, n_vals, life)))
-        if t%100 == 0:
-            save(t,life, life_shape)
+        life = calc_next_frame(life, L, boundary)
 
-run(T_max, life.ravel(), L, life_shape)
+    save(t, life, filenames)
+
+
+filenames = []
+run(T_max, life, L, boundary, filenames)
+# %%
+import imageio
+with imageio.get_writer('test.gif', mode='I') as writer:
+    for frame in filenames:
+        image = imageio.imread(frame)
+        writer.append_data(image)
+writer.close()
--- zad1/t_e1.py
+++ zad1/t_e1.py
@@ -1,53 +1,93 @@
-#%%
+# %%
+import imageio
+from itertools import repeat
+from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("/nfs/work/2022/tp449528/lib")
-from numba import jit
-from itertools import repeat
 
 rule_str = input("rule:")
 
 sustain_rule, born_rule = rule_str.split('/')
-sustain_rule = list(map(int,sustain_rule))
-born_rule = list(map(int,born_rule))
+sustain_rule = list(map(int, sustain_rule))
+born_rule = list(map(int, born_rule))
 
-@jit(nopython = True)
-def evlove(pos, L, n_val, curr, born_rule, sustain_rule):
-    if pos//L == 0 or (pos%L)==0 or n_val in born_rule or (n_val in sustain_rule and curr == 1):
-        return 1
-    else:
-        return 0
-print("xd")
-#TASK 2
-#init
-life_shape = (256,512)
+# TASK extra 1
+# init
+life_shape = (256, 512)
 L = life_shape[1]
-life = np.random.randint(0,2,life_shape)
+life = np.random.randint(0, 2, life_shape)
 T_max = 1000
+boundary = np.zeros(life_shape)
+for i in range(life_shape[0]):
+    boundary[i, 0] = 1
+    boundary[i, -1] = 1
+for i in range(life_shape[1]):
+    boundary[0, i] = 1
+    boundary[-1, i] = 1
+# %%
 
-print("xdxd")
-#%%
-def save(time, life, life_shape):
-    fig = life.reshape(life_shape)
-    plt.imshow(fig, interpolation = 'none')
-    plt.savefig(f"./zad1/imgs/life_{time}.png")
 
-@jit(nopython = True)
-def calc_neighbour(life,L):
-    neighbour_val = np.roll(life,1) + np.roll(life,-1) \
-            + np.roll(life,L) + np.roll(life,-L) \
-            + np.roll(life,L+1) + np.roll(life,L-1)\
-            + np.roll(life,-L+1) + np.roll(life,-L-1)
+def save(time, life, filenames):
+    plt.imshow(life, interpolation='none')
+    plt.savefig(f"./imgs/life_{time}.png")
+    filenames.append(f"./imgs/life_{time}.png")
+
+
+@jit(nopython=True)
+def calc_neighbour(life, L):
+    life = life.ravel()
+    neighbour_val = np.roll(life, -1) + np.roll(life, 1) + np.roll(life, L) + np.roll(
+        life, -L) + np.roll(life, L+1) + np.roll(life, -L+1) + np.roll(life, L-1) + np.roll(life, -L-1)
+    # neighbour_val = np.roll(life, (1,0), axis=(0,1)) + np.roll(life, (-1,0), axis=(0,1)) \
+    #     + np.roll(life, (0,-1), axis=(0,1)) + np.roll(life, (0,1), axis=(0,1)) \
+    #     + np.roll(life, (1,1), axis=(0,1)) + np.roll(life, (-1,1), axis=(0,1))\
+    #     + np.roll(life, (-1,-1), axis=(0,1)) + np.roll(life, (1,-1), axis=(0,1))
     return neighbour_val
 
 
-def run(time, life,L, life_shape, tot_length, born_rule, sustain_rule):
+def calc_born(life_shape, born_rule, neighbours):
+    born = np.zeros(life_shape, dtype=bool)
+    for i in born_rule:
+        born = born | (neighbours == i)
+    return born
+
+
+def calc_survived(life, sustain_rule, neighbours):
+    survived = np.zeros(life.shape, dtype=bool)
+    for i in sustain_rule:
+        survived = survived | ((neighbours == i) & (life == 1))
+    return survived
+
+
+def calc_next_frame(life, L, boundary, born_rule, sustain_rule):
+    n_vals = calc_neighbour(life, L)
+    n_vals = n_vals.reshape(life.shape)
+
+    born = calc_born(life.shape, born_rule, n_vals)
+    survived = calc_survived(life, sustain_rule, n_vals)
+
+    newlife = survived | born | (boundary == 1)
+    return newlife.astype(int)
+
+
+def run(time, life, L, boundary, filenames, born_rule, sustain_rule):
+
     for t in range(time):
-        n_vals = calc_neighbour(life,L)
+        if t % 100 == 0:
+            save(t, life, filenames)
 
-        life = np.array(list(map(evlove, np.arange(tot_length),repeat(tot_length), n_vals, life, repeat(born_rule), repeat(sustain_rule))))
-        if t%100 == 0:
-            save(t,life, life_shape)
+        life = calc_next_frame(life, L, boundary, born_rule, sustain_rule)
 
-run(T_max, life.ravel(), L, life_shape, life_shape[0]*life_shape[1], born_rule, sustain_rule)
+    save(t, life, filenames)
+
+
+filenames = []
+run(T_max, life, L, boundary, filenames, born_rule, sustain_rule)
+# %%
+with imageio.get_writer('test.gif', mode='I') as writer:
+    for frame in filenames:
+        image = imageio.imread(frame)
+        writer.append_data(image)
+writer.close()
